cedbe59f038393c8a92ddac003fe8c4993f64e15
Harsh Panchal <harshkumarpanchal04@gmail.com>
2024-04-23 18:15:37 +0530

    Models added

---
+++ TicTacToe-main/README.md
@@ -0,0 +1,15 @@
+
+Structure of the Game
+Winning Criteria
+Draw condition of the game
+*Requirement Gathering*  :- Functionalities which our system will support 
+	Scope of the problem statement [Visualise the System:-]
+	Size of the board is N X N
+	No. of Players = N - 1
+	Every Player can choose their own Symbol [ No Two should have the same symbol]
+	Only 1 bot allowed per game
+	Who will make first move?
+		At the start of the game we will randomise the Player List
+	How to decide the winner of the game?
+		A player will be considered as the winner if they get same symbol across any row any column of any diagonal
+	Game ends as soon as someone wins or Game Draws.[Game Theory and Heuristic Algorithms]
---
+++ TicTacToe-main/src/main/java/Main.java
@@ -0,0 +1,39 @@
+import controllers.GameController;
+import exceptions.InvalidMoveException;
+import models.*;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws InvalidMoveException {
+        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        GameController gameController = new GameController();
+
+        int dimension = 3;
+        List<Player> players = List.of(
+                new Player("Divyanshu", new Symbol('X'), PlayerType.HUMAN),
+                new Bot("Computer", new Symbol('O'), PlayerType.BOT, BotDifficultyLevel.EASY)
+        );
+
+        Game game = gameController.startGame(dimension, players);
+        //gameController.printBoard(game);
+
+        while (game.getGameState().equals(GameState.IN_PROGRESS)) {
+            //1. print the board.
+            gameController.printBoard(game);
+
+            //2. Player's turn
+            gameController.makeMove(game);
+        }
+
+        if (!gameController.checkState(game).equals(GameState.ENDED)) {
+            game.setGameState(GameState.DRAW);
+            System.out.println("Game DRAW");
+        } else {
+            gameController.printBoard(game);
+            System.out.println("Player " + gameController.getWinner(game).getName() + " is the winner");
+        }
+    }
+}
